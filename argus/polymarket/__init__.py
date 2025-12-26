adbc2c3d79116051ef318d027401956d1e2a2766
Sal <salmanfaris2005@hotmail.com>
2025-12-26 06:23:54 +0400

    Refactor PolymarketDispatcher: remove old stub and implement new structure based on polymarket_direct

--- argus/polymarket/__init__.py
+++ argus/polymarket/__init__.py
@@ -1,6 +1,9 @@
 """
 Refreshed Polymarket Dispatcher based on the polymarket_direct module. For the old version
 see https://github.com/The-Sal/Argus/tree/legacy/polymarket-dispatcher
+
+The below code removes the entire old stub with a new implementation based on polymarket_direct.
+In future version this documentation referencing the old dispatcher will be removed.
 """
 import logging
 from argus import polymarket_direct
