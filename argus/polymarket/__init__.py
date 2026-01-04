5ba9e7ea96fb318c9ffd06764f3247b011d6c93f
Sal <salmanfaris2005@hotmail.com>
2026-01-04 04:21:17 +0400

    polymarket_direct/__init__: * Added back py-clob-client * Patching clob to work with WireProxy * Added integration with CLOB for API derivation and caching. Tested caching is appropriate * Updated logging of websocket * Added documentation about ping * Refactored unsubscribe_from_market_data to add type hints to make API more explicit * Added consitency between `POLYMARKET_ENABLE_UNSUB_PATCH` on/off modes
    polymarket_direct/_example
    * Removed JSON dump for all BTC Markets
    * Removed websocket.enableTrace default
    
    wireproxy/wrapper
    * Added start_proxy_and_return_bind for integration with httpx
    
    polymarket_direct/_examples/unsub_test
    * Script to test Polymarket unsubscription (works!)

--- argus/polymarket/__init__.py
+++ argus/polymarket/__init__.py
@@ -5,7 +5,7 @@ see https://github.com/The-Sal/Argus/tree/legacy/polymarket-dispatcher
 The below code removes the entire old stub with a new implementation based on polymarket_direct.
 In a future version this documentation referencing the old dispatcher will be removed.
 """
-import socket
+# import socket
 # import requests
 # from websocket import WebSocketApp
 # from argus.capital import DomainCache
