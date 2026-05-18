62e725a09e9fe2091226594328d2f28e344ff970
TheSal <70804343+The-Sal@users.noreply.github.com>
2026-05-18 17:37:43 +0100

    chore: bump version to 0.4.1

--- argus/__init__.py
+++ argus/__init__.py
@@ -10,7 +10,7 @@ The following sources are available through Argus:
 - Polymarket (polymarket): Real-time market data and order ability from Polymarket prediction markets.
 """
 
-__version__ = '0.3.1'
+__version__ = '0.4.1'
 
 # Warning: Kept for compatibility with a cache mechanism; do not remove
 from argus._argus_utils import throw_fuss
